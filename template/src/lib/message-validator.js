ccad1e261154968b36a29e9e7cd201b0e3053c4c
utnim2 <mintugogoi567@gmail.com>
2024-06-21 03:21:17 +0530

    log the name of the message that is absent

--- template/src/lib/message-validator.js
+++ template/src/lib/message-validator.js
@@ -1,7 +1,35 @@
+const yaml = require('js-yaml');
+const fs = require('fs');
 const path = require('path');
 const AsyncApiValidator = require('asyncapi-validator');
 
 // Try to parse the payload, and increment nValidated if parsing was successful.
+
+function validateMessageIdentifiers(asyncApiSpec) {
+  const messages = asyncApiSpec.components.messages;
+  const missingNames = [];
+
+  for (const [messageName, messageObj] of Object.entries(messages)) {
+    if (!messageObj.name) {
+      missingNames.push(messageName);
+    }
+  }
+
+  return missingNames;
+}
+
+function performPreGenValidation(asyncApiFilePath) {
+  const fileContents = fs.readFileSync(asyncApiFilePath, 'utf8');
+  const asyncApiSpec = yaml.load(fileContents);
+
+  const missingNames = validateMessageIdentifiers(asyncApiSpec);
+
+  if (missingNames.length > 0) {
+    const errorMessage = `msgIdentifier "name" does not exist for message(s): ${missingNames.join(', ')}`;
+    throw new Error(errorMessage);
+  }
+}
+
 module.exports.validateMessage = async (
   payload,
   channelName,
@@ -10,11 +38,17 @@ module.exports.validateMessage = async (
   nValidated = 0
 ) => {
   const asyncApiFilePath = path.resolve(__dirname, '../../asyncapi.yaml');
-  const va = await AsyncApiValidator.fromSource(asyncApiFilePath, {
-    msgIdentifier: 'name',
-  });
-  va.validate(messageName, payload, channelName, operation);
-  nValidated++;
-
-  return nValidated;
+  try {
+    performPreGenValidation(asyncApiFilePath);
+    const va = await AsyncApiValidator.fromSource(asyncApiFilePath, {
+      msgIdentifier: 'name',
+    });
+    va.validate(messageName, payload, channelName, operation);
+    nValidated++;
+  
+    return nValidated;
+  } catch(error) {
+    error.name = 'AsyncAPIValidationError';
+    throw error;
+  }
 };
