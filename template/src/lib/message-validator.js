13738be38a26f7de1f610f279f93762ad511fa7c
utnim2 <mintugogoi567@gmail.com>
2024-07-02 02:02:34 +0530

    added the param for validation of messageId

--- template/src/lib/message-validator.js
+++ template/src/lib/message-validator.js
@@ -18,7 +18,10 @@ function validateMessageIdentifiers(asyncApiSpec) {
   return missingNames;
 }
 
-function performPreGenValidation(asyncApiFilePath) {
+function performPreGenValidation(asyncApiFilePath, messageRuntimeValidation) {
+  if (messageRuntimeValidation === false) {
+    return;
+  }
   const fileContents = fs.readFileSync(asyncApiFilePath, 'utf8');
   const asyncApiSpec = yaml.load(fileContents);
 
@@ -26,20 +29,31 @@ function performPreGenValidation(asyncApiFilePath) {
 
   if (missingNames.length > 0) {
     const errorMessage = `msgIdentifier "name" does not exist for message(s): ${missingNames.join(', ')}`;
-    throw new Error(errorMessage);
+    const warningMessage = 'If you are not able to modify your AsyncAPI document to add missing message IDs, then disable runtime validation logic by passing parameter messageRuntimeValidation set to false';
+    const error = new Error(errorMessage);
+    error.warning = warningMessage;
+    error.name = 'AsyncAPIValidationError';
+    throw error;
   }
 }
 
-module.exports.validateMessage = async (
+async function validateMessage (
   payload,
   channelName,
   messageName,
   operation,
-  nValidated = 0
-) => {
+  messageRuntimeValidation,
+  nValidated = 0,
+) {
+
+  if (messageRuntimeValidation === false) {
+    return nValidated + 1;
+  }
+
   const asyncApiFilePath = path.resolve(__dirname, '../../asyncapi.yaml');
+
   try {
-    performPreGenValidation(asyncApiFilePath);
+    performPreGenValidation(asyncApiFilePath, messageRuntimeValidation);
     const va = await AsyncApiValidator.fromSource(asyncApiFilePath, {
       msgIdentifier: 'name',
     });
@@ -48,7 +62,12 @@ module.exports.validateMessage = async (
   
     return nValidated;
   } catch(error) {
-    error.name = 'AsyncAPIValidationError';
+    // error.name = 'AsyncAPIValidationError';
+    // console.log("erooror", error)
     throw error;
   }
+};
+
+module.exports = {
+  validateMessage
 };
